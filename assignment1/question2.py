273e0c45455d2cd520c9401f7960f2986da3bc1a
David Fan <davidfan@college.harvard.edu>
2014-02-24 01:23:43 +0400

    Done

--- assignment1/question2.py
+++ assignment1/question2.py
@@ -1,3 +1,6 @@
+import json
+from PIL import Image
+
 class Article:
     '''
     Question 2a
@@ -21,7 +24,42 @@ class Article:
             - modify load to load info about related picture (if it exists)
             - modify show to also show the related picture (if it exist)
     '''
-    pass
+    
+    def __init__(self, headline, content, creator, related_image):
+        self.headline = headline
+        self.content = content
+        self.creator = creator
+        self.related_image = Picture(related_image)
+
+    def show(self):
+        print("Headline: " + self.headline + 
+              "\nAuthor: " + self.creator + 
+              "\nContent: " + self.content)
+        self.related_image.show()
+
+    def save(self, filename):
+        try:
+            with open(filename, 'w') as f:
+                f.write(json.dumps([{"headline":self.headline},
+                                    {"content": self.content},
+                                    {"creator": self.creator},
+                                    {"related_image": self.related_image.__str__()}]))
+        except IOError as e:
+            print "IOError {0}: {1}".format(e.errno, e.strerror)
+            return
+
+    def load(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                encoded = f.read();
+        except IOError as e:
+            print "IOError {0}: {1}".format(e.errno, e.strerror)
+            return
+        loaded = json.loads(encoded)
+        self.headline = loaded['headline']
+        self.content  = loaded['content']
+        self.creator  = loaded['creator']
+        self.related_image = Picture(loaded['related_image'])
 
 class Picture:
     '''
@@ -32,4 +70,17 @@ class Picture:
          Methods
             - show (show image)
     '''
-    pass
+    def __init__(self, image_file, creator):
+        self.image_file = image_file
+        self.creator = creator
+
+    def show(self):
+        try:
+            im = Image.open(self.image_file)
+            im.show()
+        except IOError as e:
+            print "IOError {0}: {1}".format(e.errno, e.strerror)
+            return
+
+    def __str__(self):
+        return self.image_file
